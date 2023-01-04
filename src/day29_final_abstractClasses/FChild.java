0ab5b44be5eaec1a3520d68f7f475687c320b204
S-Yucel <113521835+S-Yucel@users.noreply.github.com>
2023-01-04 14:44:27 +0300

    abstract

---
+++ src/day29_final_abstractClasses/FChild.java
@@ -0,0 +1,24 @@
+package day29_final_abstractClasses;
+
+public class FChild extends DParent {
+    @Override
+    public void method1() {
+
+    }
+
+    @Override
+    public void method2() {
+
+    }
+
+    /*
+    bugune kadar olusturdugumuz standart classları abstraction ile tanımlamak
+    istedigimizde abstract olamyan class demek yerine concrete class diyoruz
+
+    eger concrete bir class, abstract bir classı parent edinirse, java class
+    deklerasyonunun altını
+    kırmızı cızıp, parent abstract class(lar)daki methodları
+    kendisine IMPLEMENT(uyarlama) etmesini ister
+
+     */
+}
---
+++ src/day29_final_abstractClasses/GAbbstractChild.java
@@ -0,0 +1,19 @@
+package day29_final_abstractClasses;
+
+public abstract class GAbbstractChild extends DParent{
+
+    /*
+    Abstact bir classın, abstract bir child classı oldugunda,
+    parent clasdaki methodları IMPLEMENT etmek zorunda DEGİLDİR.
+     */
+
+    public void method1(){
+
+    }
+
+    public abstract void method4();
+
+    public void method5(){
+
+    }
+}
